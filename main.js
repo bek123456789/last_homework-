f11652d8d854a98d1c27f2330dabcfef31c27a01
bek123456789 <redmirating@gmail.com>
2024-04-16 21:11:48 +0500

    done

---
+++ main.js
@@ -0,0 +1,106 @@
+
+// 1
+// let number1 = +prompt('Birinchi sonni kiriting')
+// let number2 = +prompt('Ikkinchi sonni kiriting')
+
+// function sumFunc(a, b) {
+//     return a + b
+// }
+// console.log(sumFunc(number1, number2));
+
+
+
+
+// 2
+
+// function changeType() {
+//     let first = '12';
+//     let changeType = Number(first);
+//     console.log(typeof changeType);
+// }
+// changeType()
+
+
+
+
+// 3
+
+
+
+// function randomNumber() {
+
+//     let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+//     if (randomNumber % 2 == 0) {
+//         console.log(randomNumber + `juft son`);
+//     } else {
+//         console.log(randomNumber + `toq son`);
+//     }
+
+
+// }
+// randomNumber()
+
+
+
+// 4
+
+
+
+// function checkWord(word) {
+//     if (word === "Mars") {
+//         return word;
+//     } else {
+//         return "Mars It school";
+//     }
+// }
+
+// let wordPromp = prompt("So'z kiriting:");
+// let result = checkWord(wordPromp);
+// console.log(result);
+
+
+
+// 5
+
+
+// function createArray() {
+//     let wordsArray = [];
+//     for (let i = 0; i < 5; i++) {
+//         let word = prompt("So'z kiriting:");
+//         wordsArray.push(word);
+//     }
+//     return wordsArray;
+// }
+
+// function filterWords(array) {
+//     return array.filter(word => word.length >= 5);
+// }
+
+// let inputArray = createArray();
+// let resultArray = filterWords(inputArray);
+// console.log(resultArray);
+
+
+
+
+// 6
+
+
+
+
+// let juftSonlar = [];
+// let toqSonlar = [];
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         juftSonlar.push(i); 
+//     } else {
+//         toqSonlar.push(i); 
+//     }
+// }
+
+
+// console.log('Juft sonlar:', juftSonlar);
+// console.log('Toq sonlar:', toqSonlar);
